f4dc2fd46cce0c11c8fdb8c77a41b94acee8af6d
brooke <brooke@meetup.com>
2020-01-19 18:11:26 -0500

    fix photo

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,9 +5,14 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div className={styles.homepage}>
-        <div className={styles.portrait}>
-          <img src="/photo.jpeg" alt="Portrait of me" width="110%" />
+        {/* Photo */}
+        <div>
+          <div className={styles.portrait}>
+            <img src="/photo.jpeg" alt="Portrait of me" width="100%" />
+          </div>
         </div>
+
+        {/* Overlay text */}
         <div className={styles.overlay}>
           <h1 className={styles.title}>Brooke Weil</h1>
           <h2 className={styles.subtitle}>
