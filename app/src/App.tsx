4c02edf9517241ccb5d7474f47e6e2f0fad734e8
brooke <brooke@meetup.com>
2020-01-19 17:50:13 -0500

    moar

---
+++ app/src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./App.module.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className={styles.homepage}>
+        <div className={styles.portrait}>
+          <img src="/photo.jpeg" alt="Portrait of me" width="110%" />
+        </div>
+        <div className={styles.overlay}>
+          <h1 className={styles.title}>Brooke Weil</h1>
+          <h2 className={styles.subtitle}>
+            <a href="https://www.linkedin.com/in/brookeweil/">LinkedIn</a>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
